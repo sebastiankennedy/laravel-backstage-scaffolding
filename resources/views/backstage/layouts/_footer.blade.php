c97057209ddbfc4b54158cad2976525200701512
卢艺源 <sebastiankennedy@foxmail.com>
2018-05-15 15:37:39 +0800

    多用户认证 - 新增控制器、视图、资源文件、路由

--- resources/views/backstage/layouts/_footer.blade.php
+++ resources/views/backstage/layouts/_footer.blade.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
         Not only high distinction, but have dream.
     </div>
-    <strong>Copyright © 2016 - {{date('Y')}} <a href="">Laravel 有限公司</a>.</strong>
+    <strong>Copyright © 2016 - {{date('Y')}} <a href="">Laravel Tutorial</a>.</strong>
 </footer>
